602c3cd4e2a0195037c06878107252074df16729
Ali Hamzehpour <alihamzehpour2002@gmail.com>
2023-11-24 11:18:50 +0330

    Implement input buildings and resources

--- Map-Reduce/src/main.cpp
+++ Map-Reduce/src/main.cpp
@@ -19,11 +19,26 @@ std::vector<std::filesystem::path> find_buildings(std::string starting_path){
     return buildings;
 }
 
+std::vector<std::string> read_space_separated_input(std::string msg){
+    std::cout << msg << std::endl;
+    std::string input;
+    getline(std::cin, input);
+    std::istringstream iss(input);
+    std::vector<std::string> tokens;
+    std::string cur_token;
+    while (getline(iss, cur_token, ' ')){
+        tokens.push_back(cur_token);
+    }
+    return tokens;
+
+}
+
 void print_buildings(std::vector<std::filesystem::path> buildings){
     std::cout << "Buildings:" << std::endl;
-    for (auto building : buildings){
+    for (int i = 0; i < buildings.size(); i++){
+        std::filesystem::path building = buildings[i];
         std::string building_name = building.filename();
-        std::cout << "\t" << building_name << std::endl;
+        std::cout << "\t" << i + 1 << "- " << building_name << std::endl;
     }
 }
 
@@ -36,6 +51,9 @@ int main(int argc, char* argv[]){
     std::string starting_path = argv[1];
     std::vector<std::filesystem::path> buildings = find_buildings(starting_path);
     print_buildings(buildings);
+    std::vector<std::string> wanted_buildings = read_space_separated_input("Enter the name of buildings you want to process:");
+    std::vector<std::string> wanted_resources = read_space_separated_input("Enter the name of resources you want to process:");
+
 
 
     return EXIT_SUCCESS;
